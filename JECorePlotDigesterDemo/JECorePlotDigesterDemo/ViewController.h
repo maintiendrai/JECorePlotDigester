f4a2e0799bc8b2b9bbce5249bf7fd615a026b26f
lilkr <huangxu.sy@star-net.cn>
2015-08-13 13:29:19 +0800

    add demo use

--- JECorePlotDigesterDemo/JECorePlotDigesterDemo/ViewController.h
+++ JECorePlotDigesterDemo/JECorePlotDigesterDemo/ViewController.h
@@ -11,7 +11,7 @@
 
 @interface ViewController : UIViewController
 
-@property (nonatomic, strong) PlotItem *detailItem;
+
 
 
 @end
--- JECorePlotDigesterDemo/JECorePlotDigesterDemo/ViewController.m
+++ JECorePlotDigesterDemo/JECorePlotDigesterDemo/ViewController.m
@@ -11,8 +11,12 @@
 #import "CurvedScatterPlot.h"
 #import "JESimplePieChart.h"
 #import "JEBarChart.h"
+#import "JECurvedPlotView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) PlotItem *pieChartDetailItem;
+@property (nonatomic, strong) PlotItem *barChartDetailItem;
 
 @end
 
@@ -21,16 +25,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//    SNEventGraphView *view = [[SNEventGraphView alloc]initWithFrame:CGRectMake(10, 50, 250, 200) data:nil];
-//    
-//    [self.view addSubview:view];
     
-//    PlotItem *plotItem = [[CurvedScatterPlot alloc]init];
+    //饼图 JESimplePieChart:
     PlotItem *plotItem = [[JESimplePieChart alloc]init];
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 200)];
     [self.view addSubview:view];
-    self.detailItem = plotItem;
-    [self.detailItem renderInView:view withTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme] animated:YES];
+    
+    //柱状图 JEBarChart
+    _barChartDetailItem = [[JEBarChart alloc]init];
+    UIView *barView = [[UIView alloc]initWithFrame:CGRectMake(0, 200, 320, 200)];
+    [self.view addSubview:barView];
+    [_barChartDetailItem renderInView:barView withTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme] animated:YES];
+    
+    
+    //折线图
+    SNEventGraphView *curvedPlotView = [[SNEventGraphView alloc]initWithFrame:CGRectMake(10, 400, 250, 200) data:nil];
+    [self.view addSubview:curvedPlotView];
+    
+    
+    self.pieChartDetailItem = plotItem;
+    [self.pieChartDetailItem renderInView:view withTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme] animated:YES];
+    
     
     // Do any additional setup after loading the view, typically from a nib.
 }

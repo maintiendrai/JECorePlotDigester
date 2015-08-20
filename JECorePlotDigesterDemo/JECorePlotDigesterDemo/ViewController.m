8f948676527864088371dcbb955b06d1b5a1d58f
lilkr <maintiendrai@gmail.com>
2015-08-20 21:28:44 +0800

    add JEControlChart

--- JECorePlotDigesterDemo/JECorePlotDigesterDemo/ViewController.m
+++ JECorePlotDigesterDemo/JECorePlotDigesterDemo/ViewController.m
@@ -12,6 +12,7 @@
 #import "JESimplePieChart.h"
 #import "JEBarChart.h"
 #import "JECurvedPlotView.h"
+#import "JEControlChart.h"
 
 @interface ViewController ()
 
@@ -22,29 +23,35 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
-    //饼图 JESimplePieChart:
-    PlotItem *plotItem = [[JESimplePieChart alloc]init];
-    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 200)];
-    [self.view addSubview:view];
-    
-    //柱状图 JEBarChart
-    _barChartDetailItem = [[JEBarChart alloc]init];
-    UIView *barView = [[UIView alloc]initWithFrame:CGRectMake(0, 200, 320, 200)];
-    [self.view addSubview:barView];
-    [_barChartDetailItem renderInView:barView withTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme] animated:YES];
-    
-    
     //折线图
-    SNEventGraphView *curvedPlotView = [[SNEventGraphView alloc]initWithFrame:CGRectMake(10, 400, 250, 200) data:nil];
-    [self.view addSubview:curvedPlotView];
+    PlotItem *plotItem = [[JEControlChart alloc]init];
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 100, 320, 150)];
+    [self.view addSubview:view];
+
+    
+//    //饼图 JESimplePieChart:
+//    PlotItem *plotItem = [[JESimplePieChart alloc]init];
+//    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 200)];
+//    [self.view addSubview:view];
+//
+//    //柱状图 JEBarChart
+//    _barChartDetailItem = [[JEBarChart alloc]init];
+//    UIView *barView = [[UIView alloc]initWithFrame:CGRectMake(0, 200, 320, 200)];
+//    [self.view addSubview:barView];
+//    [_barChartDetailItem renderInView:barView withTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme] animated:YES];
+//    
+//    
+//    //折线图
+//    SNEventGraphView *curvedPlotView = [[SNEventGraphView alloc]initWithFrame:CGRectMake(10, 300, 250, 200) data:nil];
+//    [self.view addSubview:curvedPlotView];
     
     
     self.pieChartDetailItem = plotItem;
-    [self.pieChartDetailItem renderInView:view withTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme] animated:YES];
+    [self.pieChartDetailItem renderInView:view withTheme:nil animated:YES];
     
     
     // Do any additional setup after loading the view, typically from a nib.
